76973b02a4dc324d5ac9a32d115ba3b196dad517
Joe Rutkin <rutkinj@gmail.com>
2022-11-07 16:02:56 -0800

    adds simple tests for album class

--- src/test/java/com/songrJRut/songr/SongrApplicationTests.java
+++ src/test/java/com/songrJRut/songr/SongrApplicationTests.java
@@ -1,7 +1,11 @@
 package com.songrJRut.songr;
 
+import com.songrJRut.songr.albums.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SongrApplicationTests {
@@ -10,4 +14,27 @@ class SongrApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void canMakeAlbum(){
+		Album sut = new Album("Plock","Plone", 3, 731, "URL");
+		assertNotNull(sut);
+	}
+	@Test
+	void testGetters(){
+		Album sut = new Album("Plock","Plone", 3, 731, "URL");
+		assertEquals("Plock", sut.getTitle());
+		assertEquals("Plone", sut.getArtist());
+		assertEquals(3, sut.getSongCount());
+	}
+
+	@Test
+	void testSetters(){
+		Album sut = new Album("Nonsense","No Artist", 38733, 731, "URL");
+		sut.setTitle("Plock");
+		sut.setArtist("Plone");
+		sut.setSongCount(3);
+		assertEquals("Plock", sut.getTitle());
+		assertEquals("Plone", sut.getArtist());
+		assertEquals(3, sut.getSongCount());
+	}
 }
